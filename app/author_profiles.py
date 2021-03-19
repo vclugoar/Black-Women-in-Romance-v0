03c2148840226ac1d8f313f086f8c337bbb13aed
Vianny Lugo Aracena <viannyluggo@gmail.com>
2021-03-18 22:17:03 -0400

    Update author_profiles.py

--- app/author_profiles.py
+++ app/author_profiles.py
@@ -5,8 +5,8 @@ import numpy as np
 def main():
 
 # Use the full page instead of a narrow central column
-    bookData = pd.read_csv('books.csv')
-    author_df = pd.read_csv('authors.csv')
+    bookData = pd.read_csv('app/Data/books.csv')
+    author_df = pd.read_csv('app/Data/authors.csv')
     authors = author_df['Name'].unique()
     authorNames = np.insert(authors, 0, "All")
     authorImages = author_df['Image']
