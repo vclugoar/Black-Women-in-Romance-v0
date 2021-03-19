03b6629c1227e71eee4948175a322df122d7c4a0
Vianny Lugo Aracena <viannyluggo@gmail.com>
2021-03-18 20:18:20 -0400

    br

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-import book_recommendation
+#import book_recommendation
 import book_choices
 import author_profiles
 import about 
@@ -12,7 +12,7 @@ st.set_page_config(page_title='Black Women in Romance', layout='centered',)
 PAGES = { 
     "About": about,
     "Book Catalog": book_choices,
-    "Book Recommendations": book_recommendation,  
+   # "Book Recommendations": book_recommendation,  
     "Author Spotlight": author_profiles,
     "Add Books or Authors": intake_form
 }
@@ -33,9 +33,9 @@ if selection == 'About':
 elif selection == 'Book Catalog':
     st.image('https://i.ibb.co/527LgRd/2.png', use_column_width=True)
     page.main()
-elif selection == 'Book Recommendations':
-    st.image('https://i.ibb.co/G55FZs0/3.png', use_column_width=True)
-    page.main()
+# elif selection == 'Book Recommendations':
+#     st.image('https://i.ibb.co/G55FZs0/3.png', use_column_width=True)
+#     page.main()
 elif selection == 'Author Spotlight':
     st.image('https://i.ibb.co/5618csr/4.png', use_column_width=True)
     page.main()
